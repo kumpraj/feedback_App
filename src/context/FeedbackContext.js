16a6b6ba48934a8f092681f0823edcc91a1d3223
Prajwal Kumar <kumpraj@gmail.com>
2023-01-18 23:27:40 +0530

    replaced async with promise for add feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -33,18 +33,20 @@ export const FeedbackProvider = ({ children }) => {
     })
 
     // Add feedback
-    const addFeedback = async (newFeedback) => {
-        const response = await fetch('/feedback', {
+    const addFeedback = (newFeedback) => {
+        const response = fetch('/feedback', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(newFeedback),
         })
+        .then((res) => res.json())
+        .then((data) => setFeedback([data, ...feedback]))
 
-        const data = await response.json()
+        // const data = await response.json()
 
-        setFeedback([data, ...feedback])
+        // setFeedback([data, ...feedback])
     }
 
     // delete feedback
